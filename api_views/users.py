1c5c2f474c779f466c22b0d1c816458addae8529
Pragya <pragya.dhir97@gmail.com>
2024-09-28 21:13:00 +1000

    week 2

--- api_views/users.py
+++ api_views/users.py
@@ -83,7 +83,7 @@ def login_user():
             if user and request_data.get('password') != user.password:
                 return Response(error_message_helper("Password is not correct for the given username."), 200, mimetype="application/json")
             elif not user:  # User enumeration
-                return Response(error_message_helper("Username does not exist"), 200, mimetype="application/json")
+                return Response(error_message_helper("Username does not exist. Week2 Completed"), 200, mimetype="application/json")
         else:
             if (user and request_data.get('password') != user.password) or (not user):
                 return Response(error_message_helper("Username or Password Incorrect!"), 200, mimetype="application/json")
